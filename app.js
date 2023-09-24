c75dad623cc8c8503a3275981a275369340f1ba3
nazariipastukh <nazariy2002n@gmail.com>
2023-09-24 13:24:25 +0300

    basics practice

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const fs = require('node:fs/promises')
 const path = require('node:path')
+const fs = require('fs/promises')
 
 const creator = async () => {
     try {
+        const basePath = path.join(__dirname, 'mainFolder')
+
+        await fs.mkdir(basePath, {recursive: true})
+
         const folderName = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5']
         const fileName = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt']
 
-        const filePath = path.join(__dirname, 'folder')
-
-        await fs.mkdir(filePath)
-
         await Promise.all([
-            folderName.map(async (folder) => {
-                await fs.mkdir(path.join(filePath, folder))
+            ...folderName.map(async (folder) => {
+                await fs.mkdir(path.join(basePath, folder), {recursive: true})
             }),
-
-            fileName.map(async (file) => {
-                await fs.writeFile(path.join(filePath, file), '')
+            ...fileName.map(async (file) => {
+                await fs.writeFile(path.join(basePath, file), 'file')
             })
         ])
 
-        const readdir = await fs.readdir(filePath)
+        const readDir = await fs.readdir(basePath)
 
-        await Promise.all(readdir.map(async (item) => {
-            const stat = await fs.stat(path.join(filePath, item))
-
-            console.log(stat.isFile() ? 'File' : 'Folder', item)
-        }))
+        await Promise.all([
+            await (readDir.map(async (item) => {
+                const stat = await fs.stat(path.join(basePath, item))
+                console.log(stat.isFile() ? 'File: ' : 'Folder: ', item)
+            }))
+        ])
 
     } catch (e) {
-        console.log(e)
+        console.error(e.message)
     }
 }
 
